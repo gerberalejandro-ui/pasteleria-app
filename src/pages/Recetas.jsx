81ca245d0b8e5460e2fdc69c765901d502b39c51
Alejandro Gerber <gerber.alejandro@gmail.com>
2026-05-25 14:49:32 -0300

    update

--- src/pages/Recetas.jsx
+++ src/pages/Recetas.jsx
@@ -15,7 +15,7 @@ export default function Recetas() {
 
   const [nuevaReceta, setNuevaReceta] = useState({
     nombre: "",
-    margen: 0,
+    //margen: 0,
     procedimiento: "",
     tiempo_horas: "",
     horas_luz: "",
@@ -251,7 +251,7 @@ export default function Recetas() {
 
           <input
             style={styles.input}
-            placeholder="📈 Margen Ganancia"
+            placeholder="📈 Margen %"
             type="number"
             value={nuevaReceta.margen}
             onChange={(e) =>
